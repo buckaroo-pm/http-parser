a8c9a87b3a284fe7a82f9ee93c6bb29eecbc6a86
njlr <njlr@users.noreply.github.com>
2019-01-10 12:12:53 +0000

    * Upgrades Buckaroo for Travis

--- travis/before-install-osx.sh
+++ travis/before-install-osx.sh
@@ -1,1 +1,3 @@
-#!/bin/bash
+wget https://github.com/LoopPerfect/buckaroo/releases/download/$BUCKAROO_VERSION/buckaroo-macos -O buckaroo-client
+chmod +x ./buckaroo-client
+./buckaroo-client version
